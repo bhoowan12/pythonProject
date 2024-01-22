a6d8565aa70ddc80e75c3ae5089375049fdd71e0
Bhoowan <bhoowankapoor.bk@gmail.com>
2024-01-22 11:12:30 +0200

    new commit

---
+++ .idea/celsius.py
@@ -0,0 +1,4 @@
+fahrenheit_str = input("Enter a temperature in Fahrenheit: ")
+fahrenheit = float(fahrenheit_str)
+celsius = (fahrenheit-32)*5/9
+print("The temperature in Celsius: " + str(celsius))
